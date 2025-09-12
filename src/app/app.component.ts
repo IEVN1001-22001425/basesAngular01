b71a9ace5f27693e1ac8d196818669a89cdbfba5
Margarita <gomez130798.manrique@gmail.com>
2025-09-11 21:04:51 -0600

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'basesAngular01';
+  title = 'Magui Gómez';
+
+  duplicarNumero(valor:number):number{
+    return valor*2;
+  }
+
+  pelicula={
+    titulo: "El padrino",
+    anio: 1972,
+    genero: "Crimen/Drama"
+  }
+
 }
